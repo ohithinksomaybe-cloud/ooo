7a0ebb4aac15e358dd88d3bd996e820ad8ea4ec1
ohithinksomaybe-cloud <ohithinksomaybe@gmail.com>
2026-04-30 00:42:58 +0300

    Delete src directory

--- src/util/range.js
+++
@@ -1,5 +0,0 @@
-// Returns an array of `size` integers starting at `startAt`.
-// e.g. range(2, 4) => [2, 3, 4, 5]
-const range = (startAt, size) => [...Array(size).keys()].map((i) => i + startAt);
-
-module.exports = range;
